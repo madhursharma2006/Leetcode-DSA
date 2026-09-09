6c7e11eca4ce737d8e9b1df1ccec0d2fc0ccc298
Madhur Sharma <madhursharmalpu@gmail.com>
2026-09-09 23:18:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.88%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1) return true;
+        int start=2;
+        long long end=num;
+        while(start<=end){
+            long long mid=start+(end-start)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid>num) end=mid-1;
+            else start=mid+1;
+        }
+        return false;
+        
+    }
+};
