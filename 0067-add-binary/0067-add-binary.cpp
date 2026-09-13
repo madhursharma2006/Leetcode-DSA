5c13e10979c26b00733f3c5b2539061f3bd38b3d
Madhur Sharma <madhursharmalpu@gmail.com>
2026-09-13 23:07:03 +0530

    Time: 3 ms (17.96%), Space: 9 MB (56.5%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int sum=0;
+        int carry=0;
+        string res="";
+        while(i>=0 || j>=0){
+            sum=carry;
+            if(i>=0){
+                sum+=a[i]-'0';
+                i--;
+            }
+            if(j>=0) {sum+=b[j]-'0';
+            j--;
+            }
+            res.push_back((sum%2)+'0');
+            carry=sum/2;
+        }
+        if(carry) res.push_back('1');
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};
